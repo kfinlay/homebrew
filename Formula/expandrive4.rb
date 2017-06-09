205f58ddcc3f6013864537087676a36c4351f3c6
Keith Finlay <kfinlay@gmail.com>
2017-06-09 09:15:58 -0400

    Create expandrive4.rb

--- Formula/expandrive4.rb
+++ Formula/expandrive4.rb
@@ -1,9 +1,9 @@
-cask 'expandrive' do
+cask 'expandrive4' do
   version '4.3.2'
   sha256 'a3e2bfa0ce25462b27ad2d8daffbc6a79a098cda44030451b79585366ac6cbc4'
 
   url "https://updates.expandrive.com/apps/expandrive/v/#{version.dots_to_hyphens}/download.dmg"
-  name 'ExpanDrive'
+  name 'ExpanDrive4'
   homepage 'http://updates.expandrive.com/release_notes/expandrive'
 
   app 'ExpanDrive.app'
